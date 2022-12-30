6b3639d638be69bac0e6e840c02f83ad558ce8b5
janahmedprg <jan.ahmed.prg@gmail.com>
2022-12-30 14:09:01 -0500

    fixed youtube gallery

--- app.js
+++ app.js
@@ -4,25 +4,51 @@ function update() {
   const rect = elem.getBoundingClientRect();
   const wrap = container.getBoundingClientRect();
   var opac = 100;
-  if(86 > rect.bottom - wrap.top){
-    opac = (rect.bottom - wrap.top)/2.5+37;
-    if(opac<= 70){
-      container.style.opacity = '70%';
+  if (86 > rect.bottom - wrap.top) {
+    opac = (rect.bottom - wrap.top) / 2.5 + 37;
+    if (opac <= 70) {
+      container.style.opacity = "70%";
+    } else {
+      container.style.opacity = opac.toString() + "%";
     }
-    else{
-      container.style.opacity = opac.toString() + '%';
-    }
-  }
-  else{
-    opac = (rect.bottom - wrap.top)/2.5+37;
-    if(opac>= 100){
-      container.style.opacity = '100%';
-    }
-    else{
-      container.style.opacity = opac.toString() + '%';
+  } else {
+    opac = (rect.bottom - wrap.top) / 2.5 + 37;
+    if (opac >= 100) {
+      container.style.opacity = "100%";
+    } else {
+      container.style.opacity = opac.toString() + "%";
     }
   }
-
 }
 
 document.addEventListener("scroll", update);
+
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("demo");
+  let captionText = document.getElementById("caption");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+  captionText.innerHTML = dots[slideIndex - 1].alt;
+}
