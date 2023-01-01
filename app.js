a4bbfe319fd50357ede60a7eed56bbf8e3669fc0
janahmedprg <jan.ahmed.prg@gmail.com>
2023-01-01 14:36:38 -0500

    fixed new stuff

--- app.js
+++ app.js
@@ -52,3 +52,36 @@ function showSlides(n) {
   dots[slideIndex - 1].className += " active";
   captionText.innerHTML = dots[slideIndex - 1].alt;
 }
+
+function plusSlides1(n) {
+  showSlides1((slideIndex1 += n));
+}
+
+function currentSlide1(n) {
+  showSlides1((slideIndex1 = n));
+  $('.vids')[0].contentWindow.postMessage('{"event":"command","func":"'+'stopVideo'+'","args":""}', '*');
+}
+
+
+
+function showSlides1(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides1");
+  let dots = document.getElementsByClassName("demo1");
+  let captionText = document.getElementById("caption1");
+  if (n > slides.length) {
+    slideIndex1 = 1;
+  }
+  if (n < 1) {
+    slideIndex1 = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex1 - 1].style.display = "block";
+  dots[slideIndex1 - 1].className += " active";
+  captionText.innerHTML = dots[slideIndex1 - 1].alt;
+}
